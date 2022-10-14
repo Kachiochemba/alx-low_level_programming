54391cdd7ddee3eb3948c8b4c7c394fe7b010f56
Kachiochemba <ochembaonyekachi.t@gmail.com>
2022-10-14 07:46:07 -0700

    new commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase, except for q and e.
+ *
+ * Return: Always 0.
+*/
+
+int main(void)
+{
+	int A;
+
+	for (A = 0; A <= 10; A++)
+	{
+	printf("%d\n", A);
+	}
+	return (0);
+}
