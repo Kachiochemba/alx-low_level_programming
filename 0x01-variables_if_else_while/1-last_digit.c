7851100da1493e764a7a8373d23b2d1933cdafaa
Kachiochemba <ochembaonyekachi.t@gmail.com>
2022-10-14 06:42:34 -0700

    new commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - prints last digit of a number
  *
@@ -28,7 +29,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %dand is 0\n", n, n % 10);
+		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
 	return (0);
 }
