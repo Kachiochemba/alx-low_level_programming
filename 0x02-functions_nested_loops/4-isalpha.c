20b4ee03a067f05383eb90d67b597e085aacf3c6
Kachiochemba <ochembaonyekachi.t@gmail.com>
2022-10-18 11:04:05 -0700

    new

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Short description, single line
+ * @c: contains value to be compared
+ *
+ * Return: Always 0.
+ */
+
+int _isalpha(int c)
+{
+	if ((c > 'a' && c < 'z') || (c > 'A' && c < 'Z'))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
