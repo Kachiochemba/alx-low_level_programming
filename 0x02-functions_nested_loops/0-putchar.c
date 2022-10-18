7a5ea966a409bf802d40762d3988c0369ce5a827
Kachiochemba <ochembaonyekachi.t@gmail.com>
2022-10-18 08:54:36 -0700

    new commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print putchar
+ *
+ * Return : Always 0
+*/
+
+int main(void)
+{
+	char Character[] = "_putchar";
+
+	printf("%s\n", Character);
+	return (0);
+}
